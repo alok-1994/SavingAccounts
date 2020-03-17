c96039139671e92042d3eb15bbc6cb353bd401a7
Alok kumar <alok.123068@gmail.com>
2020-03-17 11:29:52 +0530

    Initial Commit

---
+++ savingAccount/savingAccount/src/test/java/com/demo/savingaccount/savingAccount/SavingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.savingaccount.savingAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
